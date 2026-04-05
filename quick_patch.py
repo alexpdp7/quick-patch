a1ec70ae77306f72e3c944fbef1ab094612e96e8
alex <alex@pdp7.net>
2026-04-05 21:43:38 +0200

    Create a format-patch style message

--- quick_patch.py
+++ quick_patch.py
@@ -20,7 +20,7 @@ class Repo:
             encoding="UTF8",
         ).stdout
 
-    def make_patch(self, path, patched):
+    def make_patch(self, path, patched, author, message):
         with tempfile.TemporaryDirectory() as tempdir:
             tempdir = pathlib.Path(tempdir)
             subprocess.run(
@@ -29,12 +29,24 @@ class Repo:
             )
             try:
                 (tempdir / path).write_text(patched)
-                return subprocess.run(
-                    ["git", "diff"],
+                subprocess.run(
+                    ["git", "add", "."],
                     check=True,
-                    stdout=subprocess.PIPE,
                     encoding="UTF8",
                     cwd=tempdir,
+                )
+                subprocess.run(
+                    ["git", "commit", f"--author={author}", "-m", message],
+                    check=True,
+                    encoding="UTF8",
+                    cwd=tempdir,
+                )
+                return subprocess.run(
+                    ["git", "format-patch", "--stdout", "HEAD^"],
+                    check=True,
+                    encoding="UTF8",
+                    cwd=tempdir,
+                    stdout=subprocess.PIPE,
                 ).stdout
             finally:
                 subprocess.run(
@@ -48,9 +60,26 @@ class Repo:
                     ],
                     check=True,
                 )
+                subprocess.run(
+                    [
+                        "git",
+                        f"--git-dir={self.repo}",
+                        "branch",
+                        "--delete",
+                        "--force",
+                        tempdir.name,
+                    ],
+                    check=True,
+                )
 
 
 repo = Repo()
+
+
+def _get_form_field(input_, name: str):
+    values = input_.get(name.encode("UTF8"), [])
+    assert len(values) < 2
+    return values[0].decode("UTF8") if values else None
 
 
 def quick_patch_app(environ, start_response):
@@ -59,30 +88,52 @@ def quick_patch_app(environ, start_response):
     input_ = urllib.parse.parse_qs(input_)
     global repo
 
-    posted_files = input_.get(b"file", [])
-    assert len(posted_files) < 2
-    file = posted_files[0].decode("UTF8") if posted_files else repo.get_file(path)
+    posted_file = _get_form_field(input_, "file")
+    file = posted_file or repo.get_file(path)
 
     patch = ""
-    if posted_files:
-        patch = repo.make_patch(
-            path, posted_files[0].decode("UTF8").replace("\r\n", "\n")
-        )
+    if posted_file:
+        author = f"{_get_form_field(input_, 'name') or 'unknown'} <{_get_form_field(input_, 'email') or 'unknown@example.com'}>"
+        message = _get_form_field(input_, "summary") or "Apply feedback"
+        description = _get_form_field(input_, "description")
+        if description:
+            message += "\n\n"
+            message += description
+        patch = repo.make_patch(path, file.replace("\r\n", "\n"), author, message)
 
     out = f"""
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <style>
-          textarea {{
+          textarea[name=file] {{
             width: 100%;
             height: 80%;
+          }}
+
+          label {{
+            display: block;
           }}
         </style>
       </head>
       <body>
         <form action="{environ["PATH_INFO"]}" method="POST">
-          <textarea name="file">{html.escape(file)}</textarea>
+          (All fields are optional.)
+          <label>Your name
+            <input name="name">
+          </label>
+          <label>Your email address (optional, will be published)
+            <input name="email" type="email">
+          </label>
+          <label>Summary of the change
+            <input name="summary" size="50">
+          </label>
+          <label>Description of the change
+            <textarea name="description" cols="72" rows="5"></textarea>
+          </label>
+          <label>Proposed change
+            <textarea name="file">{html.escape(file)}</textarea>
+          </label>
           <input type="submit">
         </form>
         <pre>{html.escape(patch)}</pre>
